bfbcdc1c9574b31da8d5f450ef43823e5594da53
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-08-14 10:25:51 -0400

    Altered to use express.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 const Todo = require('./models/Todo');
 
-const server = http.createServer( (req, res) => {
+// Create the server and call it app
+const app = express();
+const port = 3000;
+
+
+// Tells express to handle GET requests with this function at the route '/'
+app.get('/todos', (req, res) => {
+// const server = http.createServer( (req, res) => {
     console.log(`You've got a new request!`);
     const allTodos = Todo.getAll();
     allTodos
         .then(data => {
-            console.log('\n======================================')
+            console.log('\n======================================\n')
             console.log(data);
-            res.end(`<h1> ${JSON.stringify(data)} </h1>`);
+            res.json(data);
+            // res.end(JSON.stringify(data));
         })
-
 });
-// console.log(server);
-server.listen(3000);
+
+// server.listen(3000);
+app.listen(port, () => console.log(`App listening on port: ${port}`));
