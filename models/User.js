6754f9c895909724106c9d29124ce2a4abbb426f
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-08-15 11:15:16 -0400

    Added todos to single user queries

--- models/User.js
+++ models/User.js
@@ -3,19 +3,30 @@ const db = require('../db');
 
 function getAll() {
     return db.any(`SELECT * FROM users`)
-        .catch(err => {
+        .catch( err => {
             console.log('Error getting users. ');
             console.log(err);            
         })
 }
 
 function getOne(id) {
-return db.one(
-    `
-    SELECT * FROM USERS
-        WHERE id = $1
-    `, [id])
-    .catch(err => {
+    return db.one(`
+        SELECT * FROM USERS
+            WHERE id = $1
+        `, [id])
+    .then( user => {
+        const todos = db.any(`
+        SELECT * FROM todos WHERE id = $1
+        `, [id])
+        .then( todosForUser => {
+            console.log(todosForUser);
+            user.todos = todosForUser;
+            return user;
+        })
+        return todos;
+    })
+
+    .catch( err => {
         console.log('Error getting user');
         console.log(err);
     })
